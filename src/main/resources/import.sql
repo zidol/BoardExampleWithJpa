de284de89b6c2a2c26cd64f0241ad7c803f6aa1b
zidol <ajh3166@naver.com>
2022-05-23 22:19:22 +0900

    스프링 실행 시 초기 데이터 용 입력 sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into member (member_id, CREATED_DATE  , UPDATED_DATE  , CREATED_BY  , UPDATED_BY  ,  ACCOUNT_ID  , ACCOUNT_TYPE  , NICKNAME  , QUIT  ) values (1, now(), now(), 'Anonymous', 'Anonymous', 'user1', 'REALTOR', 'son', FALSE);
+
+insert into member (member_id, CREATED_DATE  , UPDATED_DATE  , CREATED_BY  , UPDATED_BY  ,  ACCOUNT_ID  , ACCOUNT_TYPE  , NICKNAME  , QUIT  ) values (2, now(), now(), 'Anonymous', 'Anonymous', 'user2', 'LESSOR', 'dol', FALSE);
+
+insert into member (member_id, CREATED_DATE  , UPDATED_DATE  , CREATED_BY  , UPDATED_BY  ,  ACCOUNT_ID  , ACCOUNT_TYPE  , NICKNAME  , QUIT  ) values (3, now(), now(), 'Anonymous', 'Anonymous', 'user3', 'LESSEE', 'zeeee', FALSE);
