3cf5457ec7b229bb571dfec36daec9773a9af0b9
zidol <ajh3166@naver.com>
2022-05-24 00:43:06 +0900

    member 계정 타입 수정 auditoraware 로직 추가

--- src/main/java/com/callbus/community/domain/AccountType.java
+++ src/main/java/com/callbus/community/domain/AccountType.java
@@ -1,5 +1,13 @@
 package com.callbus.community.domain;
 
 public enum AccountType {
-    LESSOR, REALTOR, LESSEE
+    LESSOR("임대인"), REALTOR("공인 중개사"), LESSEE("임차인");
+
+    private String desc;
+    AccountType(String desc) {
+        this.desc = desc;
+    }
+    public String getDesc() {
+        return desc;
+    }
 }
--- src/main/java/com/callbus/community/service/common/CommonAwareAudit.java
+++ src/main/java/com/callbus/community/service/common/CommonAwareAudit.java
@@ -1,17 +1,45 @@
 package com.callbus.community.service.common;
 
+import com.callbus.community.domain.Member;
+import com.callbus.community.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class CommonAwareAudit implements AuditorAware<String> {
-    public CommonAwareAudit() {
-    }
+
+    private final MemberRepository memberRepository;
 
     public Optional<String> getCurrentAuditor() {
-        //TODO : 유저 id 들어가도록 로직 작성해야함
-        return Optional.of("Anonymous");
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        Long userId = null;
+        if (authorization == null) {
+            return Optional.of("Anonymous");
+        } else if (authorization.startsWith("Realtor ")) {
+            userId = Long.valueOf(authorization.substring("Realtor ".length()));
+        } else if (authorization.startsWith("Lessor ")) {
+            userId = Long.valueOf(authorization.substring("Lessor ".length()));
+        } else {
+            userId = Long.valueOf(authorization.substring("Lessee ".length()));
+        }
+        try {
+            Member member = memberRepository.findById(userId).orElse(null);
+            return Optional.of(member.getAccountId());
+        } catch (Exception e) {
+            return Optional.of("Anonymous");
+        }
     }
 }
