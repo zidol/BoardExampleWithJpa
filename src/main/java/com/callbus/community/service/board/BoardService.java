c6c293c7b135bcf403a04faf4e0ba28363e32ba6
zidol <ajh3166@naver.com>
2022-05-27 17:40:38 +0900

    게시판 등록, 수정, 삭제 인터페이스

--- src/main/java/com/callbus/community/service/board/BoardService.java
+++ src/main/java/com/callbus/community/service/board/BoardService.java
@@ -1,18 +1,49 @@
 package com.callbus.community.service.board;
 
-import com.callbus.community.dto.board.BoardForm;
+import com.callbus.community.domain.Board;
+import com.callbus.community.dto.board.BoardDto;
+import com.callbus.community.dto.board.BoardUpdateForm;
 import com.callbus.community.dto.board.BoardListDto;
 import com.callbus.community.dto.board.BoardSearchForm;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 /**
  * 게시판 관련 인터페이스
  */
 public interface BoardService {
 
-    public Page<BoardListDto> getBoardList(BoardSearchForm boardSearchForm, Pageable pageable, Long memberId);
+    /**
+     * 게시판 목록 조회(페이징)
+     *
+     * @param boardSearchForm
+     * @param pageable
+     * @param memberId
+     * @return Page<BoardListDto>
+     * @throws Exception
+     */
+    public Page<BoardListDto> getBoardList(BoardSearchForm boardSearchForm, Pageable pageable, Long memberId) throws Exception;
+
+    /**
+     * 게시판 등록
+     * @param boardDto
+     * @throws Exception
+     */
+    public Board insertBoard(BoardDto boardDto) throws Exception;
+
+    /**
+     * 게시판 수정
+     * @param boardForm
+     * @throws Exception
+     */
+    public Board updateBoard(BoardUpdateForm boardForm) throws Exception;
+
+    /**
+     * 게시판 삭제
+     *
+     * @param boardForm
+     * @throws Exception
+     */
+    public Board deleteBoard(BoardUpdateForm boardForm) throws Exception;
 
 }
